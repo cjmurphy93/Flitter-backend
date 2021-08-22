5f45e414bd9683bf5296ccbec395a4cebdd17aa4
cjmurphy93 <cjmurph93@gmail.com>
2021-08-22 00:22:09 -0400

    updated like/dislike functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +7,31 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+const db = admin.firestore()
+
+exports.addLike = functions
+    .region('us-east4')
+    .firestore.document('/posts/{postId}/likes/{userId}')
+    .onCreate((snap, context) => {
+        return db
+            .collection('posts')
+            .doc(context.params.postId)
+            .update({
+                numLikes: admin.firestore.FieldValue.increment(1),
+            })
+    })
+
+exports.deleteLike = functions
+    .region('us-east4')
+    .firestore.document('/posts/{postId}/likes/{userId}')
+    .onDelete((snap, context) => {
+        return db
+            .collection('posts')
+            .doc(context.params.postId)
+            .update({
+                numLikes: admin.firestore.FieldValue.increment(-1),
+            })
+    })
